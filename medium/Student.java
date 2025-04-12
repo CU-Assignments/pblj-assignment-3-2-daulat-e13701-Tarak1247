5b1dea125db5e9c39cbd9acb128b9de8ee95248f
Tarak Aggarwal <116876777+Tarak1247@users.noreply.github.com>
2025-04-12 13:31:19 +0530

    Create Student.java

---
+++ medium/Student.java
@@ -0,0 +1,13 @@
+import javax.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+
+    // Getters and Setters
+}
+  
